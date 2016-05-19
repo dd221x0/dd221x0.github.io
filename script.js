a2087c7dc4f480650701a7926e7c00bd949004f5
shaeredal <shaeredal@gmail.com>
2016-05-19 03:16:37 +0300

    small fixes and some js

---
+++ script.js
@@ -0,0 +1,12 @@
+// $("#tetrahedron").draggable({
+// 	distance: 128,
+// 	containment: "window",
+// 	opacity: 0.35,
+// 	refreshPositions: true
+// });
+
+// $("#strip").draggable({
+// 	axis: "y",
+// 	containment: "document",
+// 	refreshPositions: true
+// })
