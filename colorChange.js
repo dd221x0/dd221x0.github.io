e4772eb8dc2ca825dfe02e07e4287d8964ed62c9
shaeredal <shaeredal@gmail.com>
2016-10-12 01:06:08 +0300

    code is refactored, links highliting added

--- colorChange.js
+++ colorChange.js
@@ -1,40 +1,68 @@
 window.onload = () => {
 
 	function getRandomColor() {
-    	var letters = '0123456789ABCDEF';
-    	var color = '#';
+    	let letters = '0123456789ABCDEF';
+    	let color = '#';
     	for (var i = 0; i < 6; i++ ) {
     	    color += letters[Math.floor(Math.random() * 16)];
     	}
     	return color;
 	}
 
-	let parts = document.querySelectorAll("#tetrahedron div");
+	function setHover(elements) {
+		elements.forEach((el) => {
+			el.onmouseover = () => {
+				el.style.color = hoverColor;
+			};
+			el.onmouseout = () => {
+				el.style.color = '#AAAAAA';
+			};
+		}
+	)
+	}
 
-	let color = '#AAAAAA';
-
-	parts.forEach((el) =>{
+	function setTetrahedronHover(elements){
+		elements.forEach((el) => {
 				el.onmouseover = () => {
-					parts.forEach((e) => {
+					elements.forEach((e) => {
 						e.style.borderBottomColor = color;
 					}
 					)
 				};
 				el.onmouseout = () => {
-					parts.forEach((e) =>{
+					elements.forEach((e) =>{
 						e.style.borderBottomColor = '#000';
 					}
 					) ;
 				};
 			});
+	}
 
-	parts.forEach((element) => {
-		element.addEventListener('click', (e)=>{
-			color = getRandomColor();
-			parts.forEach((el) =>{
+	function setTetrahedronEvent(elements){
+		elements.forEach((el) => {
+		el.addEventListener('click', (e)=>{
+			hoverColor = color = getRandomColor();
+			elements.forEach((el) =>{
 				el.style.borderBottomColor = color;
 			});
 			e.stopPropagation();
 		})
 	})
+	}
+
+	function tetrahedronInit(elements){
+		setTetrahedronHover(elements);
+		setTetrahedronEvent(elements);
+	}
+
+	let parts = document.querySelectorAll("#tetrahedron div");
+	let socials = document.querySelectorAll("#social a");
+	let email = document.querySelectorAll("#email a");
+
+	let color = '#AAAAAA';
+	let hoverColor = '#333333';
+	
+	tetrahedronInit(parts);
+	setHover(socials);
+	setHover(email);
 }
