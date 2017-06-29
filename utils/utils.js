b4efea32af113623d0cf882d8832d308dc52ab07
shaeredal <shaeredal@gmail.com>
2017-06-30 02:53:37 +0300

    Add validation.

--- utils/utils.js
+++ utils/utils.js
@@ -11,22 +11,54 @@ function get_hash_part(string, partsCount, part){
 	if (!part) {
 		part = 1;
 	}
-	if (partsCount < part){
-		//exception
-	}
 	var password_hash = md5(string);
 	var part_len = Math.floor(password_hash.length / partsCount);
 	return password_hash.substring((part - 1) * part_len, part * part_len);
+}
+
+var saveOldValue = function(event){
+	event.target.oldValue = event.target.value;
+}
+
+var numberFilter = function(event, additionalCheck){
+	var elem = event.target;
+	var regex = /^\d*$/;
+	if(!regex.test(elem.value) 
+		|| !(+elem.value > 0 || elem.value === "")
+		|| (additionalCheck && !additionalCheck(event))){
+		elem.value = elem.oldValue;
+		event.returnValue = false;
+		event.preventDefault();
+	}
+	else{
+		saveOldValue(event);
+	}
 }
 
 submit.onclick = function(){
 	resultElem.innerHTML = get_hash_part(stringElem.value, partsCountElem.value, partElem.value);
 }
 
-partsCountElem.onkeydown = function(){
+partsCountElem.onfocus = saveOldValue;
 
+partElem.onfocus = saveOldValue;
+
+partsCountElem.oninput = function(event){
+	numberFilter(event)
+	var elem = event.target;
+	if(elem.value > 32){
+		elem.value = 32;
+	}
+	if(elem.value === ""){
+		partElem.value = "";
+	}
+	else if(+elem.value < +partElem.value){
+		partElem.value = elem.value;
+	}
 }
 
-partElem.onkeyup = function(){
-	
+partElem.oninput = function(event){
+	numberFilter(event, function(event){
+			return +event.target.value <= +partsCountElem.value;
+		});
 }
