9fa1b22da78e7b612493ae789e395037e90d6dd1
ITRANSITION\d.danilovich <d.danilovich@itransition.com>
2018-06-12 22:24:05 +0300

    howmuchisleft page is added

---
+++ howmuchisleft/scripts.js
@@ -0,0 +1,8 @@
+let endTime = new Date(2019, 5, 13);
+let curentTime;
+
+
+function getCurrentTime()
+{
+	let url = "http://api.timezonedb.com/v2/get-time-zone?key=692FL2W84HRV&by=zone&zone=Europe/Minsk&format=json"
+};
