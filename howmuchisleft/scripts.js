57cb2fd741250077a44f2b634fb69f69a12b438f
shaeredal <shaeredal@gmail.com>
2018-06-13 04:16:36 +0300

    try to use api

--- howmuchisleft/scripts.js
+++ howmuchisleft/scripts.js
@@ -2,7 +2,22 @@ let endTime = new Date(2019, 5, 13);
 let curentTime;
 
 
-function getCurrentTime()
+function setCurrentTime()
 {
-	let url = "http://api.timezonedb.com/v2/get-time-zone?key=692FL2W84HRV&by=zone&zone=Europe/Minsk&format=json"
+	let timeApiUrl = "http://api.timezonedb.com/v2/get-time-zone?key=692FL2W84HRV&by=zone&zone=Europe/Minsk&format=json"
+
+	let xhr = new XMLHttpRequest();
+	xhr.open('GET', timeApiUrl, true);
+
+	xhr.onreadystatechange = (e) =>
+	{
+		console.log(e);
+	};
+
+	xhr.send();
 };
+
+function setTime(e)
+{
+
+}
