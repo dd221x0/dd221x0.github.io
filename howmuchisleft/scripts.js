0ff89c988dc18dc02d396fa8915973df5e35a2ff
shaeredal <shaeredal@gmail.com>
2018-06-14 05:21:24 +0300

    add timer

--- howmuchisleft/scripts.js
+++ howmuchisleft/scripts.js
@@ -1,23 +1,51 @@
 let endTime = new Date(2019, 5, 13);
-let curentTime;
+let currentTime;
+
+let timer;
 
 
-function setCurrentTime()
+function setCurrentTime(callback)
 {
 	let timeApiUrl = "http://api.timezonedb.com/v2/get-time-zone?key=692FL2W84HRV&by=zone&zone=Europe/Minsk&format=json"
 
 	let xhr = new XMLHttpRequest();
+
+	let resultRecieved = false;
+
 	xhr.open('GET', timeApiUrl, true);
 
 	xhr.onreadystatechange = (e) =>
 	{
-		console.log(e);
+		if(resultRecieved || (e.target.status === 200 && !e.target.response))
+		{
+			return;
+		}
+
+		resultRecieved = true;
+
+		if(e.target.status === 200)
+		{
+			callback(new Date(JSON.parse(e.target.response).formatted));
+		}
+		else
+		{
+			setTimeout(setCurrentTime, 1000);
+		}
 	};
 
 	xhr.send();
 };
 
-function setTime(e)
+function setTimer(time)
 {
+	let difference = (endTime - time)/1000;
 
+	timer = setInterval(() => 
+		{
+			document.getElementById("time").innerHTML = `${~~(difference/(24*60*60))} ${~~(difference/(60*60))%24}:${~~(difference/60)%60}:${~~difference%60}`;
+			difference--;
+		}
+		, 1000);
 }
+
+setCurrentTime(setTimer);
