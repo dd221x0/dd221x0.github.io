f056c9341cf5bec3dbe128c9d16f462dd31545fc
shaeredal <shaeredal@gmail.com>
2018-07-08 05:17:09 +0300

    add functional of gitbranchname

--- utils/gitbranchname/scripts.js
+++ utils/gitbranchname/scripts.js
@@ -2,9 +2,12 @@ var stringElem = document.getElementById("string");
 var resultElem = document.getElementById("result");
 
 function stingToGitBranchName(string){
-	let result = "";
-	result += string.replace(new RegExp(" +", "g"), "-");
-
+	let result = string
+		.replace(/[\~\^:\[\]\\]/g, "")
+		.replace(/\.{2,}/g, ".")
+		.replace(/\/\.+/g, "/")
+		.replace(/( |(\.lock)|\/)+$/g, "")
+		.replace(/ +/g, "-");
 
 	return result;
 };
